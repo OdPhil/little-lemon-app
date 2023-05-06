0c2734755ae250ddf619d74d486fd4978cff8576
Odphil <phodinakachi@gmail.com>
2023-05-07 00:20:38 +0100

    Added Hero Section

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import Nav from './Nav';
+import React, { useState } from 'react';
+import './App.css';
+import Logo from './img/logo.png';
+
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
-      <Nav />
+    <div className="header">
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </div>
   );
 }
+
 
 export default Header;
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import Hero from './img/restauranfood.jpg';
 
 function Main(props) {
   return (
-   <main></main>
+   <main>
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1 className="main-heading">Little Lemon</h1>
+        <h2 className="sub-heading">Chicago</h2>
+        <p className="hero-text">We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        <button className="reserve-btn">Reserve a Table</button>
+      </div>
+      <div className="hero-image">
+        <img src={Hero} alt="Hero Image" />
+      </div>
+    </div>
+    <div className='highlights'>
+
+    </div>
+    <div className='testimonials'>
+
+    </div>
+    <div className='about'>
+
+    </div>
+   </main>
   );
 }
 
